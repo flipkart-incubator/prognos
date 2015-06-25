d3a32268e477a733b121f889b184c5e7b1534e68
ajayrfhp <ajayrfhp1710@gmail.com>
2015-06-25 17:37:54 +0530

    final arima and croston

--- src/test/scala/com/prognos/forecast/HoltWinterTest.scala
+++ src/test/scala/com/prognos/forecast/HoltWinterTest.scala
@@ -2,9 +2,9 @@ package com.prognos.forecast
 
 import breeze.linalg.DenseVector
 import com.prognos.Series
-import com.prognos.testdata.{TouristsData, AirPassengerData}
+import com.prognos.testdata.TouristsData
 import com.prognos.util.DoubleUtil
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 
 class HoltWinterTest extends FlatSpec with Matchers {
   it should "calculate forecasts using HoltWinters method with alpha=0.025, beta=0.023, gamma=0.0001" in {
@@ -15,6 +15,9 @@ class HoltWinterTest extends FlatSpec with Matchers {
     forecasts.length should equal(4)
     //forecasts.map(DoubleUtil.round(_, 2)) should equal(DenseVector(58.82, 39.08, 47.07, 51.02)) // expected output from same values for R model
     forecasts.map(DoubleUtil.round(_, 2)) should equal(DenseVector(67.47, 50.91, 60.33, 66.46)) // captures seasonal trend but not same as R output
+
+
+
   }
 
 }
